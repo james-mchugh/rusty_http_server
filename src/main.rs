ea9782d20fdd7bfb758b25ebac424ac5d6e3b23d
James McHugh <mchugh.james1@gmail.com>
2025-04-07 22:46:15 -0400

    Made echo function use HTTP response format

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use std::io::{Read, Write};
 use std::net;
-use std::thread;
 
 fn echo(mut stream: net::TcpStream) {
     let mut buffer: [u8; 512] = [0; 512];
-    println!(
-        "Incoming TCP connection from: {}",
-        stream.peer_addr().unwrap()
-    );
-    // todo: add error handling
-    while stream.read(&mut buffer).expect("read failed") > 0 {
-        stream.write(&buffer).expect("write failed");
+
+    match stream.peer_addr() {
+        Ok(addr) => println!("Incoming TCP connection from: {}", addr),
+        Err(_) => println!("Cannot get peer address"),
     }
+
+    // todo: add error handling
+    stream.read(&mut buffer).expect("read failed");
+
+    stream
+       
+        .write("HTTP/1.1 200 OK\r\n\r\n".as_ref())
+        .expect("Failed to write to socket");
+    // stream.write(&buffer).expect("Failed to write to socket");
 
     println!("Connection closed!");
 }
@@ -19,7 +24,7 @@ fn echo(mut stream: net::TcpStream) {
 fn main() {
     let listener = net::TcpListener::bind("127.0.0.1:8080").expect("Could not bind on port 8080");
     println!("Listening on: {}", listener.local_addr().unwrap());
-    let mut handlers: Vec<thread::JoinHandle<()>> = Vec::new();
+    let mut handlers = Vec::new();
     for stream in listener.incoming() {
         //  todo: add error handling
         handlers.push(std::thread::spawn(move || {
